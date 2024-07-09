bdecd3501f693feb638a655cc822b83da2bf295e
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2024-07-09 21:46:18 +0200

    install react-router-dom and setup routes

--- src/App.js
+++ src/App.js
@@ -4,28 +4,69 @@ import './_app.scss';
 
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
-import CategoriesBar from './components/categoriesBar/CategoriesBar';
-import Video from './components/video/Video';
 import HomeScreen from './screens/homeScreen/HomeScreen';
 import LoginScreen from './screens/loginScreen/LoginScreen';
 
-const App = () => {
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Navigate,
+} from 'react-router-dom';
+
+const Layout = ({ children }) => {
 	const [sidebar, toggleSidebar] = useState(false);
 
 	const handleToggleSidebar = () => toggleSidebar((value) => !value);
 
 	return (
-		// <>
-		// 	<Header handleToggleSidebar={handleToggleSidebar} />
-		// 	<section className='app__container'>
-		// 		<Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
-		// 		<Container fluid className='app__main'>
-		// 			<HomeScreen />
-		// 		</Container>
-		// 	</section>
-		// </>
-		<LoginScreen />
+		<>
+			<Header handleToggleSidebar={handleToggleSidebar} />
+			<section className='app__container'>
+				<Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
+				<Container fluid className='app__main'>
+					{children}
+				</Container>
+			</section>
+		</>
 	);
+};
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<>
+			<Route
+				path='/'
+				element={
+					<Layout>
+						<HomeScreen />
+					</Layout>
+				}
+			/>
+			<Route
+				path='/auth'
+				element={
+					<>
+						<LoginScreen />
+					</>
+				}
+			/>
+			<Route
+				path='/search'
+				element={
+					<Layout>
+						<h1>Search</h1>
+					</Layout>
+				}
+			/>
+			<Route path='*' element={<Navigate to='/' />} />
+		</>
+	)
+);
+
+const App = () => {
+	return <RouterProvider router={router} />;
 };
 
 export default App;
