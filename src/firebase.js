bf48dcf53bba79655da2555daa5357e7bb0641ce
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2024-07-10 20:44:33 +0200

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCKZMKB3Uwf4aWh51138lVjPJg9nN1F-hs',
+	authDomain: 'n-you-tube.firebaseapp.com',
+	projectId: 'n-you-tube',
+	storageBucket: 'n-you-tube.appspot.com',
+	messagingSenderId: '1044462741075',
+	appId: '1:1044462741075:web:66df575e7ffee4eee508a6',
+};
+
+firebase.initializeApp(firebaseConfig);
